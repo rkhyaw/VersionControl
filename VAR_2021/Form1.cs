974ad2b47ffebb5423ebdfc4761432ac834c390f
Dénes Benedek <benedek.d@outlook.com>
2021-11-26 16:13:52 +0100

    4. felaadat vége

--- VAR_2021/Form1.cs
+++ VAR_2021/Form1.cs
@@ -12,9 +12,14 @@ namespace VAR_2021
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
