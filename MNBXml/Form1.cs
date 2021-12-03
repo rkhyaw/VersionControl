bb44eb03a1a2fedc7a6f5b40cbd2e29c3549ce93
Dénes Benedek <benedek.d@outlook.com>
2021-12-03 18:01:14 +0100

    3

--- MNBXml/Form1.cs
+++ MNBXml/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using MNBXml.Entities;
 using MNBXml.MBNServiceReference;
 
@@ -14,16 +15,36 @@ namespace MNBXml
 {
     public partial class Form1 : Form
     {
-        BindingList<RateData> _rates;
+        BindingList<RateData> _rates = new BindingList<RateData>();
 
         public Form1()
         {
-            InitializeComponent();
-            getRates();
+            InitializeComponent();            
+            loadXML(getRates());
             dataGridView1.DataSource = _rates;
         }
 
-        private void getRates()
+        private void loadXML(string xmlstring)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlstring);
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date=DateTime.Parse(item.GetAttribute("date"));
+                var childElement = (XmlElement)item.ChildNodes[0];
+                r.Currency = childElement.GetAttribute("curr");
+                decimal unit = decimal.Parse(childElement.GetAttribute("unit"));
+                r.Value = decimal.Parse(childElement.InnerText);
+                if(unit!=0)
+                {
+                    r.Value = r.Value / unit;
+                }
+                _rates.Add(r);
+            }
+        }
+
+        private string getRates()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody req = new GetExchangeRatesRequestBody();
@@ -31,7 +52,7 @@ namespace MNBXml
             req.startDate = "2020-01-01";
             req.endDate = "2020-06-30";
             var response = mnbService.GetExchangeRates(req);
-            var result = response.GetExchangeRatesResult;
+            return response.GetExchangeRatesResult;
         }
     }
 }
