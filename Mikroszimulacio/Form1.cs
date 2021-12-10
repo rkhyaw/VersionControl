af7d112b3017bcf99294c0f076f24e5c4ccd913c
Dénes Benedek <benedek.d@outlook.com>
2021-12-10 18:31:28 +0100

    szimulacio1

--- Mikroszimulacio/Form1.cs
+++ Mikroszimulacio/Form1.cs
@@ -26,6 +26,43 @@ namespace Mikroszimulacio
             Population = GetPopulation(@"D:\temp\nép-teszt.csv");
             BirthProbabilities = GetBirthProbabilities(@"D:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"D:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SzimulaciosLepes(Population[i], year);
+                }
+
+                int ferfiakszama = (from x in Population where x.Gender == Gender.Male select x).Count();
+                int nokszama = (from x in Population where x.Gender == Gender.Female select x).Count();
+
+                Console.WriteLine(string.Format("Év: {0} Férfiak: {1} Nők: {2}", year, ferfiakszama, nokszama));
+            }
+
+        }
+
+        private void SzimulaciosLepes(Person p, int year)
+        {
+            if (!p.IsAlive) return;
+            int kor = year - p.BirthYear;
+            double halalozasvalsz = (from x in DeathProbabilities where x.Gender == p.Gender && x.Age == kor select x.P).FirstOrDefault();
+            double vltln = rng.NextDouble();
+            if (vltln <= halalozasvalsz) p.IsAlive = false;
+
+            if(p.IsAlive&&p.Gender==Gender.Female)
+            {
+                double szuletesvalsz = (from x in BirthProbabilities where x.Age == kor select x.P).FirstOrDefault();
+                vltln = rng.NextDouble();
+                if (vltln <= szuletesvalsz)
+                {
+                    Person baba = new Person();
+                    baba.BirthYear = year;
+                    baba.NbrOfChildren = 0;
+                    baba.Gender = (Gender)rng.Next(1, 3);
+                    Population.Add(baba);
+                }
+            }
         }
 
         private List<BirthProbabilities> GetBirthProbabilities(string csvpath)
