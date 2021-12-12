eee58aee7531b0755f96bee51018ef90c56c3a86
Dénes Benedek <denes.benedek@stud.uni-corvinus.hu>
2021-12-12 19:51:42 +0100

    carcs

---
+++ progtervmintak/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using progtervmintak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progtervmintak.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics graphics)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
